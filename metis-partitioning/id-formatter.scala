0de48e0f13363635caf7bd161e3e63022a54774c
Anil Pacaci <apacaci@uwaterloo.ca>
2017-07-28 17:43:20 -0400

    Twitter graph specific changes for metis formatter

--- metis-partitioning/id-formatter.scala
+++ metis-partitioning/id-formatter.scala
@@ -8,8 +8,18 @@ import org.apache.spark.SparkConf
 object MetisIDFormatter {
 
     // parse a single line, ignores the first part and creates and edge list
-    def parseEdges(l : String) : Array[String] = {
+    def parseSNBEdges(l : String) : Array[String] = {
         val edges = l.split("\\|", 3)
+        val outgoing = edges(1).split(" ").filter(!_.isEmpty).map{ edge => edge.split(",")(1) }
+        
+        val incoming = edges(2).split(" ").filter(!_.isEmpty).map{ edge => edge.split(",")(1) }
+
+        return outgoing ++ incoming
+    }
+
+    // parse a single line, ignores the first part and creates and edge list
+    def parseTwitterEdges(l : String) : Array[String] = {
+        val edges = l.split(" ")
         val outgoing = edges(1).split(" ").filter(!_.isEmpty).map{ edge => edge.split(",")(1) }
         
         val incoming = edges(2).split(" ").filter(!_.isEmpty).map{ edge => edge.split(",")(1) }
@@ -22,9 +32,9 @@ object MetisIDFormatter {
         //val sc = new SparkContext(conf)
 
         // read input file
-        val adjacencyText = sc.textFile("hdfs://192.168.152.200:9000/datasets/sf10_updates/adjacency_full")
+        val adjacencyText = sc.textFile("hdfs://192.168.152.200:9000/datasets/sf1000_updates_friendship/adjacency_format")
         // trim unnecessary information
-        val adjacency = adjacencyText.map( l => (l.split("\\|")(0), parseEdges(l) ) )
+        val adjacency = adjacencyText.map( l => (l.split("\\|")(0), parseSNBEdges(l) ) )
 
         // METIS needs undirected graph, therefore we will add reverse of the each original edge
         val undirectedEdges = adjacency.flatMap(vertex => {
@@ -39,7 +49,7 @@ object MetisIDFormatter {
         val lookup = adjacency.map( t => t._1 ).zipWithIndex.map( t => (t._1, t._2 + 1))
 
         //generate oldid - newid lookup table on one pass
-        lookup.repartition(1).saveAsObjectFile("hdfs://192.168.152.200:9000/datasets/sf10_updates/lookup")
+        lookup.saveAsObjectFile("hdfs://192.168.152.200:9000/datasets/twitter_rv/metis-lookup")
 
         // since lookup table is relatively small, we can broadcast it
         val lookupMap = sc.broadcast(lookup.collectAsMap)
@@ -53,20 +63,20 @@ object MetisIDFormatter {
         })
 
         // now we can output in METIS compatible format, eliminatng source id since METIS implicitly assumes line number is a source id
-        val metisAdjacency = adjacencyWithIdentifiers.map( l => l._2.mkString(" ")).coalesce(1)
+        val metisAdjacency = adjacencyWithIdentifiers.map( l => l._2.mkString(" "))
         val header = sc.parallelize(Seq( vertexCount + " " + (edgeCount / 2) )).coalesce(1)
 
-        header.saveAsTextFile("hdfs://192.168.152.200:9000/datasets/sf10_updates/metis-header")
-        metisAdjacency.saveAsTextFile("hdfs://192.168.152.200:9000/datasets/sf10_updates/metis")
+        header.saveAsTextFile("hdfs://192.168.152.200:9000/datasets/twitter/metis-header")
+        metisAdjacency.saveAsTextFile("hdfs://192.168.152.200:9000/datasets/twitter/metis")
     }
 
     def generatePartitionLookup(lookupPath: String, partitionPath: String, outputPath: String) {
         // here we assume that lookupPath is the path to the lookup RDD
         // partitionPath is the METIS generated output file
 
-        val lookupPath = "hdfs://192.168.152.200:9000/datasets/sf10_updates/lookup"
-        val partitionPath = "file:///home/apacaci/ldbc-gremlin/ldbc_snb_datagen/datasets/sf10_updates/metis/part-00000.part.4"
-        val outputPath = "hdfs://192.168.152.200:9000/datasets/sf10_updates/metis4-lookup"
+        val lookupPath = "hdfs://192.168.152.200:9000/datasets/sf30_updates/lookup"
+        val partitionPath = "hdfs://192.168.152.200:9000/datasets/sf30_updates//metis/metis-adjacency.txt.part.8"
+        val outputPath = "hdfs://192.168.152.200:9000/datasets/sf1000_updates_friendship/metis16-lookup"
 
         // read both files and reverse the key-value order so that METIS ids are join key
         val lookup = sc.objectFile[(String, Long)](lookupPath).map(t => (t._2, t._1))
