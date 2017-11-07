0e356e1dc253bf1d03bd5c30921c291a5adacc9f
Zhuoran Yu <z56yu@tem53.tembo-domain.cs.uwaterloo.ca>
2017-11-07 17:57:40 -0500

    shortestPath adjusted to cassandra-stats

--- scripts/shortestPathTest.groovy
+++ scripts/shortestPathTest.groovy
@@ -12,6 +12,18 @@ import org.apache.tinkerpop.gremlin.process.traversal.util.DefaultTraversalMetri
 import org.apache.tinkerpop.gremlin.process.traversal.util.TraversalMetrics
 import org.apache.tinkerpop.gremlin.structure.Graph
 
+import org.apache.commons.configuration.Configuration
+import org.apache.commons.configuration.PropertiesConfiguration
+
+import javax.management.Attribute;
+import javax.management.MBeanServerConnection;
+import javax.management.MBeanServerInvocationHandler;
+import javax.management.ObjectName;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+
+
 
 import java.util.concurrent.TimeUnit
 
@@ -21,11 +33,111 @@ import java.util.concurrent.TimeUnit
  * Helper Groovy script to run 2-hop friendship query over JanusGraph
  * It measures the time to retrieve 2-hop friendship
  */
-class PartitioningTwoHopTest {
 
-    private static String[] CSV_HEADERS = ["IID", "VERTEX_PARTITION", "TARGET_ID", "TOTAL_DURATION" ]
+public class CassandraLocalReadCounter {
 
-    static void run(Graph graph, String parametersFile, String outputFile) {
+        String cassandraJMXPort;
+        int numberOfInstances;
+
+        List<MBeanServerConnection> serverConnections;
+
+        Integer[] lastReads;
+
+        ObjectName readCountAttribute;
+
+        public CassandraLocalReadCounter(String configurationFile) {
+            Configuration configuration = new PropertiesConfiguration(configurationFile);
+
+            String[] ipAddresses = configuration.getStringArray("cassandra.host");
+            cassandraJMXPort = configuration.getString("cassandra.jmx");
+            numberOfInstances = configuration.getInt("cassandra.clustersize");
+
+            serverConnections = new ArrayList<>(numberOfInstances);
+            lastReads = new Integer[numberOfInstances];
+
+            for(String ipAddress : ipAddresses) {
+                JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://" + ipAddress + ":" + cassandraJMXPort + "/jmxrmi")
+                JMXConnector jmxc = JMXConnectorFactory.connect(url, null)
+
+                MBeanServerConnection mbsc = jmxc.getMBeanServerConnection();
+                serverConnections.add(mbsc);
+            }
+	
+	    log.info("All JMX connections initialized")
+
+            readCountAttribute = new ObjectName("org.apache.cassandra.metrics:type=Keyspace,keyspace=janusgraph,name=ReadLatency");
+
+            for(int i = 0 ; i < numberOfInstances ; i++) {
+		MBeanServerConnection mbsc = serverConnections.get(i)
+                int readCount = Integer.parseInt(mbsc.getAttribute(readCountAttribute, "Count").toString());
+                lastReads[i] = readCount;
+            }
+	
+	    log.info("Initial read counts are retrieved")
+        }
+
+        /**
+         * Iterate over all nodes in the cluster and retrieve local counts since restart
+         * @return
+         */
+        public List<Integer> getTotalReadCount() {
+            List<Integer> reads = new ArrayList();
+
+            for(int i = 0 ; i < numberOfInstances ; i++) {
+		MBeanServerConnection mbsc = serverConnections.get(i)
+                int readCount = Integer.parseInt(mbsc.getAttribute(readCountAttribute, "Count").toString());
+                reads.add(readCount);
+                lastReads[i] = readCount;
+            }
+        }
+
+        /**
+         * Iterate over all nodes in cluster and retrieve local counts in incremental manner
+         * Every call to this function updates local data structures and returns read counts since last call
+         * @return
+         */
+        public List<Integer> updateReadCount() {
+            List<Integer> reads = new ArrayList();
+
+            for(int i = 0 ; i < numberOfInstances ; i++) {
+		MBeanServerConnection mbsc = serverConnections.get(i)
+                int readCount = Integer.parseInt(mbsc.getAttribute(readCountAttribute, "Count").toString());
+                reads.add(readCount - lastReads[i]);
+                lastReads[i] = readCount;
+            }
+            return reads;
+        }
+
+    }
+
+
+
+
+
+
+class ShortestPathTest {
+
+    private static String[] CSV_HEADERS = ["IID", "TARGET_ID", "VERTEX_PARTITION", "1HOP", "2HOP", "NEIGHBOURHOOD_RETRIEVAL_DURATION",
+                                           "PROPERTIES_RETRIEVAL_DURATION", "TOTAL_DURATION",
+                                           "READS_C1",
+                                           "READS_C2",
+                                           "READS_C3",
+                                           "READS_C4",
+                                           "READS_C5",
+                                           "READS_C6",
+                                           "READS_C7",
+                                           "READS_C8",
+                                           "READS_C9",
+                                           "READS_C10",
+                                           "READS_C11",
+                                           "READS_C12",
+                                           "READS_C13",
+                                           "READS_C14",
+                                           "READS_C15",
+                                           "READS_C16"
+    ]
+
+    static void start(Graph graph, String parametersFile, String outputFile, CassandraLocalReadCounter readCounter){
 
         GraphTraversalSource g = graph.traversal()
 
@@ -46,18 +158,41 @@ class PartitioningTwoHopTest {
             String iid = current_line.split('\\|')[0]
 	    String targetId = current_line.split('\\|')[1]
 
+	    log.info("New vertex id: " + iid)
+
             DefaultTraversalMetrics metrics = g.V().has('iid', 'person:' + iid).repeat(out('knows').simplePath()).until(has('iid', 'person:'+targetId).or().loops().is(5)).limit(1).path().count(local).profile().next()
             Long vertexId = (Long) g.V().has('iid', 'person:' + iid).next().id()
             Long partitionId = getPartitionId(vertexId)
 
-            long totalQueryDurationInMicroSeconds = metrics.getDuration(TimeUnit.MICROSECONDS)
+	    log.info("Vertex: " + iid + " succesfully queried")
+
+	    long totalQueryDurationInMicroSeconds = metrics.getDuration(TimeUnit.MICROSECONDS)
+            // index 2 corresponds to valueMap step, where properties of each neighbour is actually retrieved from backend
+            long neighbourhoodRetrievalInMicroSeconds = metrics.getMetrics(2).getDuration(TimeUnit.MICROSECONDS)
+            long propertiesRetrievalInMicroSeconds = metrics.getMetrics(3).getDuration(TimeUnit.MICROSECONDS)
+
+            // index 1 corresponds to out step
+            int oneHopCount = metrics.getMetrics(1).getCount('elementCount')
+            int twoHopCount = metrics.getMetrics(2).getCount('elementCount')
+
             // index 2 corresponds to valueMap step, where properties of each neighbour is actually retrieved from backend
 
             List<String> queryRecord = new ArrayList();
             queryRecord.add(iid)
-            queryRecord.add(partitionId)
 	    queryRecord.add(targetId)
+            queryRecord.add(partitionId)
+	    queryRecord.add(oneHopCount.toString())
+     	    queryRecord.add(twoHopCount.toString())
+            queryRecord.add(neighbourhoodRetrievalInMicroSeconds.toString())
+            queryRecord.add(propertiesRetrievalInMicroSeconds.toString())
             queryRecord.add(totalQueryDurationInMicroSeconds.toString())
+
+	    List<Integer> readCounts = readCounter.updateReadCount()
+            for(int i = 0 ; i < readCounts.size() ; i++) {
+                queryRecord.add(readCounts.get(i).toString())
+            }
+
+	    log.info("Retrieved cassandra stats for query with vertex: " + iid)
 
             // add record to CSV
             csvPrinter.writeNext(queryRecord.toArray(new String[0]))
@@ -70,10 +205,13 @@ class PartitioningTwoHopTest {
         csvPrinter.close()
     }
 
-    static void run(String graphConfigurationFile, String parametersFile, String outputFile) {
+    static void run(String graphConfigurationFile, String parametersFile, String outputFile, String jmxConfigurationFile) {
+	log.info("Opening the graph file")
         Configuration graphConfig = new PropertiesConfiguration(graphConfigurationFile)
         Graph graph = JanusGraphFactory.open(graphConfig)
-        run(graph, parametersFile, outputFile)
+	log.info("JanusGraph Opened")
+	CassandraLocalReadCounter readCounter = new CassandraLocalReadCounter(jmxConfigurationFile)
+        start(graph, parametersFile, outputFile, readCounter)
     }
 
     static Long getPartitionId(Long id) {
