9c40f576e3db99a4909ae7b22733ee9f91679b11
Anil Pacaci <apacaci@uwaterloo.ca>
2019-05-10 14:44:37 -0400

    run experiments scripts updated with json config

--- containers/analytics/scripts/run_experiments.py
+++ containers/analytics/scripts/run_experiments.py
@@ -1,33 +1,15 @@
 #!/usr/bin/python
 
-import csv
+import json
 import os
 import shlex
+import sys
 
-snap_dataset	= "/home/ubuntu/datasets/twitter_rv/twitter_rv_snap/twitter_rv.net"
-adj_dataset	= "/home/ubuntu/datasets/twitter_rv/twitter_rv_adj_ec_combined.txt"
+edge_cut_sgp = ["random_ec", "ldg", "fennel", "metis"]
+vertex_cut_sgp = ["random", "dbh", "grid", "hdrf", "hybrid", "hybrid_ginger"]
 
-# snap_dataset	= "/home/ubuntu/datasets/uk2007-05/uk2007-05-snap-combined.txt"
-# adj_dataset	= "/home/ubuntu/datasets/uk2007-05/uk2007-05-adjacency-combined.txt"
+natural_algorithms = ["pagerank", "sssp"]
 
-# snap_dataset	= "/home/ubuntu/datasets/USA-road/USA-road-snap/part-00000"
-# adj_dataset	= "/home/ubuntu/datasets/USA-road/USA-road-adjacency/part-00000"
-
-result_folder	= "/home/ubuntu/experiments/powerlyra/results/twitter"
-log_folder	= "/home/ubuntu/experiments/powerlyra/logs/twitter"
-
-
-# csv file should have following headers
-# nodes 		: total number of powerlyra process to be created
-# pernode 		: # of process per physical machine
-# nedges 		: # of edges in the input graph
-# nverts 		: # of vertices in the input graph
-# algorithm 	: graph analytics algorithm to be run on the 
-# format 		: graph format, snap for vertex cut and adj_ec for edge cut
-# ingress 		: partitioning strategy
-# engine 		: plsync for vertex cut and plsyncec for edgecut
-# iterations 	: number of supersteps, set to 0 to wait for convergence
-parameters 		= "params/twitter-64-2.csv"
 
 # an object holding parameters for experiment and return the command line string to be executed
 class PowerLyraRun:
@@ -47,21 +29,36 @@ class PowerLyraRun:
 	result_file		= ""
 	log_file		= ""
 
-	def __init__(self, machines, cpu_per_node, graph_nodes, graph_edges, algorithm, graph_format, ingress, lookup, source, directed, iterations, engine):
+	def __init__(self, machines, cpu_per_node, graph_nodes, graph_edges, algorithm, ingress):
 		self.machines = machines
 		self.cpu_per_node = cpu_per_node
 		self.graph_nodes = graph_nodes
 		self.graph_edges = graph_edges
-		self.algorithm = algorithm
-		self.graph_format = graph_format
-		self.ingress = ingress
-		self.lookup = lookup
-		self.source = source
-		self.directed = directed
-		self.iterations = iterations
-		self.engine = engine
+		self.algorithm = algorithm["name"]
+		# graph format and engine is based on the cut model of the algorithm
+		if ingress["name"] in edge_cut_sgp:
+			self.graph_format = "adj_ec"
+			if algorithm["name"] in natural_algorithms:
+				self.engine = "plsyncec"
+			else:
+				self.engine = "plsync"
+		if ingress["name"] in vertex_cut_sgp:
+			self.graph_format = "snap"
+			self.engine = "plsync"
+		
+		self.ingress = ingress["name"]
+		
+		if "lookup" in ingress:
+			self.lookup = ingress["lookup"][str(machines)]
+
+		if "source" in algorithm:	
+			self.source = algorithm["source"]
+
+		if "iterations" in algorithm: 
+			self.iterations = algorithm["iterations"]
+		
 		# generate name from parameters
-		self.name = algorithm + "-" + str(machines) + "-" + engine + "-" + ingress
+		self.name = algorithm["name"] + "-" + str(machines) + "-" + ingress["name"]
 		self.result_file = os.path.join(result_folder, self.name)
 		self.log_file = os.path.join(log_folder, self.name)
 
@@ -69,7 +66,7 @@ class PowerLyraRun:
 		command = " mpiexec --mca btl_tcp_if_include eth0 "
 		command += "-n {} -npernode {} ".format(str(self.machines), str(self.cpu_per_node))
 		command += "-hostfile ~/machines "
-		command += "/home/ubuntu/powerlyra/release/toolkits/graph_analytics/{} ".format(self.algorithm)
+		command += "/sgp/powerlyra/{} ".format(self.algorithm)
 		# following command control the number of cpus per instance, it uses all cores for threading by default
 		#command += "--ncpus {} ".format(str(self.cpu_per_node))
 		# metis needs special parameters to set lookup file
@@ -97,27 +94,54 @@ class PowerLyraRun:
 		# command += "--saveprefix {} ".format(self.result_file)
 		command += "2>&1 | tee {} ".format(self.log_file)
 		return command
+
+# read the command line arguments
+if len(sys.argv) != 2:
+	print "Supply configuration file as an argument"
+	sys.exit()
+
+# second argument is the parameters file
+parameters = sys.argv[1]
 		
 # list to hold all the objects for this set of experiments
 run_list = []
 
 # generate proper host files
-os.system("/home/mpi/get_hosts > /home/mpi/machines")
+# os.system("/home/mpi/get_hosts > /home/mpi/machines")
 
-# parse csv files and populate PowerLyraRun objects
+# parse json files and populate PowerLyraRun objects
 with open(parameters, 'rb') as parameters_file:
-	parameters_csv = csv.DictReader(parameters_file)
-	for row in parameters_csv:
-		run_list.append(PowerLyraRun(int(row['nodes']), int(row['pernode']), int(row['nverts']), int(row['nedges']), row['algorithm'], row['format'], row['ingress'], row.get('lookup', ""), row.get('source', -1), row.get('directed', "true"), int(row['iterations']), row['engine']))
+	parameters_json = json.load(parameters_file)
+	run_config = parameters_json["runs"]
+	
+	## read global parameters from the json file
+	snap_dataset = run_config["snap-dataset"]
+	adj_dataset  = run_config["adj-dataset"]
+	log_folder = run_config["log-folder"]
+	result_folder = run_config["result-folder"]
+	nverts = run_config["nvertices"]
+	nedges = run_config["nedges"]
+	pernode = run_config["worker-per-node"]
+
+	## read variable parameters
+	workers_list = run_config["workers"]
+	ingress_list = run_config["ingress"]
+	algorithm_list = run_config["algorithm"]
+
+	for worker in workers_list:
+		for ingress in ingress_list:
+			for algorithm in algorithm_list:
+				run_list.append(PowerLyraRun(worker, pernode, nverts, nedges, algorithm, ingress))
 
 	# run each command one by one
 	for run in run_list:
 		print "------------"
-		print "!!! Executing: {}".format(run.produceCommandString())
-		result = os.system(run.produceCommandString())	
-		print "!!! Shell returns: {}".format(result)
+		print "!!! Executing on MPI Cluster: {}".format(run.produceCommandString())
+		# result = os.system(run.produceCommandString())
+		result = 1	
+		print "!!! MPI Cluster returns: {}".format(result)
 		print "------------"
 
-	print "All runs are executed"
+	print "All runs are completed"
 
 
