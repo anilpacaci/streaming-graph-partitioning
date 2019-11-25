677c786c27d18bb6b4fed7bfa22f74af1670cba9
Anil Pacaci <apacaci@uwaterloo.ca>
2019-11-25 16:40:50 -0500

    JanusGraph db init/loader script

--- containers/interactive/master/docker-entrypoint.py
+++ containers/interactive/master/docker-entrypoint.py
@@ -12,9 +12,6 @@ subprocess.Popen(['memcached', '-m', '5000', '-u', 'nobody'])
 # copy janusgraph-cassandra cluster settings from worker1
 subprocess.call(['scp', 'worker1:/sgp/janusgraph/conf/gremlin-server/janusgraph-cassandra-es-server.properties', '/sgp/scripts/conf/'])
 
-# execute janus initialization script
-subprocess.call(['/sgp/janusgraph/bin/gremlin.sh', '-e', '/sgp/scripts/initJanus.groovy', '/sgp/scripts/conf/janusgraph-cassandra-es-server.properties'])
-
 # easy hack to keep entry point script running forever
 while True:
 	time.sleep(60)
