2f53ebf04320bf0345f05d98bcfebe40522e82c7
Anil Pacaci <apacaci@uwaterloo.ca>
2019-12-03 11:37:20 -0500

    Interactive master - memcached for partition lookup use 16Gwq

--- containers/interactive/master/docker-entrypoint.py
+++ containers/interactive/master/docker-entrypoint.py
@@ -7,7 +7,7 @@ import subprocess
 import time
 
 # os call to start gremlin-server on each instance
-subprocess.Popen(['memcached', '-m', '5000', '-u', 'nobody'])
+subprocess.Popen(['memcached', '-m', '16384', '-u', 'nobody'])
 
 # copy janusgraph-cassandra cluster settings from worker1
 subprocess.call(['scp', 'worker1:/sgp/janusgraph/conf/gremlin-server/janusgraph-cassandra-es-server.properties', '/sgp/scripts/conf/'])
